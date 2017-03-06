7c430ccaaf988fa52b02067a1b380649e453fa64
kkk <kk@gamil.com>
2017-03-06 23:48:29 +0300

    Init commit with Nokogiri script for parse 551006's schedule

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+ my_group_doc = Nokogiri::XML(open("https://www.bsuir.by/schedule/schedule.xhtml?id=551006"))
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
