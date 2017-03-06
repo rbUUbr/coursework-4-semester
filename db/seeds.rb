1c444949d7c8d93ad6987761d0ad65934060a7d7
kkk <kk@gamil.com>
2017-03-07 01:11:56 +0300

    Edits in Nokogiri scripts(gets all time, now trying to get only labs time)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,14 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
- my_group_doc = Nokogiri::XML(open("https://www.bsuir.by/schedule/schedule.xhtml?id=551006"))
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ my_group_doc = Nokogiri::HTML(open("https://www.bsuir.by/schedule/schedule.xhtml?id=551006"))
+ lessons = []
+ times  = []
+ my_group_doc.css("span").each do |link|
+   lessons << link.content if link.text.strip.include?("(ЛР)")
+ end
+ my_group_doc.css("td").each do |link|
+      times << link.content.delete(" ").gsub(/\n/, '') if /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]-([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.match(link.text.strip)
+ end
+ File.write("/home/rbudeveloper/course_work_4_semester/db/db.txt", lessons)
+ File.write("/home/rbudeveloper/course_work_4_semester/db/db.txt", times)
