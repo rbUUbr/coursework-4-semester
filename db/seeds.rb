1ab0e7a7a576c65e2db382b8046c695e6721da22
kkk <kk@gamil.com>
2017-03-08 14:33:07 +0300

    modified Nokogiri script(need to get id of group(BSUIR API)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,6 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+URL_OF_PAGE_WITH_ALL_ID_OF_GROUPS = "https://www.bsuir.by/schedule/rest/studentGroup"
+all_groups_page = Nokogiri::XML(open(URL_OF_PAGE_WITH_ALL_ID_OF_GROUPS))
  my_group_doc = Nokogiri::HTML(open("https://www.bsuir.by/schedule/schedule.xhtml?id=551006"))
- lessons = []
- times  = []
- my_group_doc.css("span").each do |link|
-   lessons << link.content if link.text.strip.include?("(ЛР)")
- end
- my_group_doc.css("td").each do |link|
-      times << link.content.delete(" ").gsub(/\n/, '') if /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]-([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.match(link.text.strip)
- end
- File.write("/home/rbudeveloper/course_work_4_semester/db/db.txt", lessons)
- File.write("/home/rbudeveloper/course_work_4_semester/db/db.txt", times)
