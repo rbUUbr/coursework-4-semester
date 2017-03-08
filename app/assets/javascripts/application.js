21cf87608ea8bed143cc4f85f658373475a87c1e
rbUUbr <kirabsuir@gmail.com>
2017-03-08 16:39:47 +0300

    initialized Angular

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require angular
+//= require satellizer
+//= require AngularDevise
+//= require angular-rails-templates
+//= require angular-ui-router
+//= require isteven-angular-multiselect
 //= require_tree .
