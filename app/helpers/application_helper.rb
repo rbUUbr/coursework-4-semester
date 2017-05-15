e6e88cbb04f4d844d52e40f85f02c5c373fa9edf
rbUUbr <kirabsuir@gmail.com>
2017-05-15 22:38:58 +0300

    Added subgroup output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def subgroup_text(subgroup)
+    if subgroup == 1
+      return '1'
+    elsif subgroup == 2
+      return '2'
+    else
+      return 'common'
+    end
+  end
 end
